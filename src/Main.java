f1380ec7f3cc1cd2039be860a763962ddcb787b6
Sytnyk Yurii <yoursit1980@gmail.com>
2023-01-31 22:22:44 +0200

    homework16

--- src/Main.java
+++ src/Main.java
@@ -1,2 +1,20 @@
-package PACKAGE_NAME;public class Main {
+import java.util.Arrays;
+
+public class Main {
+    public static void main(String[] args) {
+        int[] array = new int[30];
+        for (int i = 0; i < array.length; i++) {
+            array[i] = ((int) (Math.random() * 31) - 15);
+        }
+        System.out.println("Было до сортировки");
+        System.out.println(Arrays.toString(array));
+
+        QuickSort.quickSort(array,0,array.length-1);
+        System.out.println("Стало после быстрой сортировки");
+        System.out.println(Arrays.toString(array));
+
+        ShakerSort.shakerSort(array);
+        System.out.println("Стало после шейкерной сортировки");
+        System.out.println(Arrays.toString(array));
+    }
 }
--- src/ShakerSort.java
+++ src/ShakerSort.java
@@ -1,2 +1,27 @@
-package PACKAGE_NAME;public class ShakerSort {
+public class ShakerSort {
+    public static void shakerSort(int[] array) {
+        int leftSide = 0;
+        int rightSide = array.length - 1;
+        int temp;
+
+        do {
+            for (int i = leftSide; i < rightSide; i++) { // Сортир. влево ищем МАКС. знач.
+                if (array[i] > array[i + 1]) {
+                    temp = array[i];
+                    array[i] = array[i + 1];
+                    array[i + 1] = temp;
+                }
+            }
+            rightSide--; // Уменьшаем количество проходов
+            for (int i = rightSide; i > leftSide; i--) { // Теперь идём в обратную сторону ищем МИН.
+                if (array[i] < array[i - 1]) {
+                    temp = array[i];
+                    array[i] = array[i - 1];
+                    array[i - 1] = temp;
+                }
+            }
+            leftSide++; // Уменьшаем количество проходов
+        } while (leftSide < rightSide);
+
+    }
 }
