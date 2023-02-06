dda1e61545dc529b2c42f3f8bf31bc215f9cb954
Sytnyk Yurii <yoursit1980@gmail.com>
2023-02-06 14:35:02 +0200

    homework16

--- src/Main.java
+++ src/Main.java
@@ -3,6 +3,7 @@ import java.util.Arrays;
 public class Main {
     public static void main(String[] args) {
         int[] array = new int[30];
+        long getTimeStart = System.currentTimeMillis();
         for (int i = 0; i < array.length; i++) {
             array[i] = ((int) (Math.random() * 31) - 15);
         }
@@ -10,10 +11,14 @@ public class Main {
         System.out.println(Arrays.toString(array));
 
         QuickSort.quickSort(array,0,array.length-1);
+        long getTimeFinish1 = System.currentTimeMillis();
+        System.out.println("Time quickSort " + (getTimeFinish1-getTimeStart+ " ms"));
         System.out.println("Became after quickSort");
         System.out.println(Arrays.toString(array));
 
         ShakerSort.shakerSort(array);
+        long getTimeFinish2 = System.currentTimeMillis();
+        System.out.println("Time shakerSort " + (getTimeFinish2-getTimeStart+ " ms"));
         System.out.println("Became after shakerSort");
         System.out.println(Arrays.toString(array));
     }
